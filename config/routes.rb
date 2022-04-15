6a86a54b764770c527151cd0171c88a38d2d6c83
EC2 Default User <ec2-user@ip-172-31-4-185.ap-northeast-1.compute.internal>
2022-04-15 15:28:14 +0000

    copy end

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  get 'users/show'
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+get "users/show" => "users#show"
   root to: "home#index"
 end
