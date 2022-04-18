74f49bcea70ec613445b916ba8ee2cd2f0f8212a
EC2 Default User <ec2-user@ip-172-31-4-185.ap-northeast-1.compute.internal>
2022-04-18 15:08:33 +0000

    Q&A

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :todos
   resources :goals
   get 'users/show'
-  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users
+
 get "users/show" => "users#show"
   root to: "home#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -38,6 +38,11 @@ ActiveRecord::Schema.define(version: 2022_04_17_094107) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "profile"
+    t.text "game_type"
+    t.text "game"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
