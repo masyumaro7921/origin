dbffb67f23f72282109e53cdd3556916a31625c1
EC2 Default User <ec2-user@ip-172-31-4-185.ap-northeast-1.compute.internal>
2022-04-18 10:40:44 +0000

    err

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :todos
+  resources :goals
   get 'users/show'
   devise_for :users, controllers: { registrations: 'users/registrations' }
 get "users/show" => "users#show"
